cf3843b47e499615696e4dfd5c73772f3928986e
Sunil1208 <skmy03@gmail.com>
2022-02-21 22:12:36 +0530

    completing google sign in

--- src/App.js
+++ src/App.js
@@ -6,19 +6,46 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
+import { auth } from './firebase/firebase.utils';
+
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop' component={ShopPage}/>
-        <Route exact path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribedFromAuth = null;
+
+  //subscribe to auth changes
+  componentDidMount() {
+    this.unsubscribedFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log('user is ', user)
+    })
+  }
+
+  //close the subscription
+  componentWillUnmount() {
+    this.unsubscribedFromAuth();
+  }
+
+  render(){
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/shop' component={ShopPage}/>
+          <Route exact path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
