9403e094a0def8886ebd8846ab6480f636c34ce9
Sunil1208 <skmy03@gmail.com>
2022-02-21 16:17:44 +0530

    creating the shop/collection page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch, Link } from 'react-router-dom';
 
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
 import './App.css';
-
-const HatsPage = () => {
-  return(
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  )
-}
-
-const TopicDetail = (props) => {
-  return (
-    <div>
-      <Link to='/'>HOME PAGE LINK</Link>
-      <button onClick={() => props.history.push('/')}>History button</button>
-      <h1>TOPIC DETAIL PAGE</h1>
-    </div>
-  )
-}
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop/hats' component={HatsPage}/>
-        <Route exact path='/topic/:topicId' component={TopicDetail}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
