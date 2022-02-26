a65617353273cd0d6ea91e763f7b9b205e7b65a0
Sunil1208 <skmy03@gmail.com>
2022-02-26 21:28:05 +0530

    added stripe integration (checkout/pay button

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = "pk_test_lMs9VbUKOsRkrLuTUStY17ZN00Obqux36q"
+
+    const onToken = token => {
+        console.log('token is ', token);
+        alert('Payment Successful');
+    }
+
+    return(
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
