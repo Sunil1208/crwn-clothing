7338012bf3a6ec951a288c0e0823d9b8fe25dc11
Sunil1208 <skmy03@gmail.com>
2022-02-26 11:54:44 +0530

    data normalization for shop data and completed collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -9,9 +9,19 @@ import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
     console.log('collection is ', collection)
+    const { title, items } = collection;
     return (
         <div className='collection-page'>
-            <h2>CATEGORY PAGE</h2>
+            <h2 className='title'>{ title }</h2>
+            <div className='items'>
+                {
+                    items.map(item => {
+                        return(
+                            <CollectionItem key={item.id} item={item} />
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 };
